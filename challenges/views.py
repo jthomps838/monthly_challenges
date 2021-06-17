cf4c603d90a197a06f74be1155230443f36819ad
Joseph Thompson <joe.thompson838@gmail.com>
2021-06-17 19:54:26 -0400

    add routes/views for each month challenge

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def challenges(request):
+    return HttpResponse('Check out all of our challenges')
+
+
+def january(request):
+    return HttpResponse("january")
+
+
+def february(request):
+    return HttpResponse("february")
+
+
+def march(request):
+    return HttpResponse("march")
+
+
+def april(request):
+    return HttpResponse("april")
+
+
+def may(request):
+    return HttpResponse("may")
+
+
+def june(request):
+    return HttpResponse("june")
+
+
+def july(request):
+    return HttpResponse("july")
+
+
+def august(request):
+    return HttpResponse("august")
+
+
+def september(request):
+    return HttpResponse("september")
+
+
+def october(request):
+    return HttpResponse("october")
+
+
+def november(request):
+    return HttpResponse("november")
+
+
+def december(request):
+    return HttpResponse("december")
